7433983b1e94d258e1f0589189eee9f8f4656ef7
Katie <kaallyn@users.noreply.github.com>
2017-09-20 11:26:26 -0500

    mobile updates

--- quality-movers/js/app.js
+++ quality-movers/js/app.js
@@ -1,46 +1,44 @@
-var jumboHeight = $('.jumbotron').outerHeight();
-function parallax(){
+$(document).ready(function(e){
+  
+  // para code
+  var jumboHeight = $('.jumbotron').outerHeight();
+  function parallax(){
     var scrolled = $(window).scrollTop();
     $('.bg').css('height', (jumboHeight-scrolled) + 'px');
-}
+  }
 
-$(window).scroll(function(e){
-    parallax();
-});
+  $(window).scroll(function(e){
+      parallax();
+  });
+  //end
 
-$(document).ready(function(e){
   $('.top-btn').addClass('animated pulse');
-});
-
-
-$(document).ready(function(){
 
   $("a").on('click', function(event) {
-
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
-
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
-   
         window.location.hash = hash;
       });
-    } // End if
+    } 
   });
-});
 
-$(function () {
-        $(window).paroller();
-});
+  $(function () {
+    $(window).paroller();
+  });
 
-// Scroll events
-jQuery(document).ready(function($){
-  var card = $('.check'),
-    offset = 1;
+  var card = $('.check');
+  var offset = .9;
 
   $(window).on('scroll', function(){
+    // if (!window.requestAnimationFrame) {
+    //   setTimeout(function(){ showBlocks(card, offset); }, 100)
+    // } else {
+    //   window.requestAnimationFrame(function(){ showBlocks(card, offset); });
+    // }
     (!window.requestAnimationFrame) 
       ? setTimeout(function(){ showBlocks(card, offset); }, 100)
       : window.requestAnimationFrame(function(){ showBlocks(card, offset); });
@@ -48,14 +46,10 @@ jQuery(document).ready(function($){
 
   function showBlocks(blocks, offset) {
     blocks.each(function(){
-      ( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).hasClass('is-hidden') ) && $(this).removeClass('is-hidden').addClass('not-hidden');
+      ( $(this).offset().top <= $(window).scrollTop() + $(window).height() * offset && $(this).hasClass('is-hidden') ) && $(this).removeClass('is-hidden').addClass('not-hidden');
     });
   }
+
+  console.log("      .--.   |V| \n     /    \\ _| / \n     q .. p \\ / \n      \\--/  // \n     __||__//\n    /.    _/ \n   // \\  / \n  //   || \n  \\\\  /  \\ \n   )\\|    | \n  / || || | \n  |/\\| || | \n     | || | \n     \\ || / \n   __/ || \\__ \n  \\____/\\____/ \n\n Greetings!");
+
 });
-
-
-
-
-
-console.log("      .--.   |V| \n     /    \\ _| / \n     q .. p \\ / \n      \\--/  // \n     __||__//\n    /.    _/ \n   // \\  / \n  //   || \n  \\\\  /  \\ \n   )\\|    | \n  / || || | \n  |/\\| || | \n     | || | \n     \\ || / \n   __/ || \\__ \n  \\____/\\____/ \n\n Greetings, Spectrum!");
-
